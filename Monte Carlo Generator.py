724a142da6be2431f44f25b2423190de66cdc69e
nstarman <nstarman@users.noreply.github.com>
2014-09-28 16:50:44 -0400

    Create Monte Carlo Generator.py

---
+++ Monte Carlo Generator.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Monte Carlo
+
+import numpy as np
+import scipy as sp
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+#
+x = [] # this should be xList from "Linear Best Fit (LSR).py"
+xError = []
+y = [] # this should be yList from "Linear Best Fit (LSR).py"
+yError = []
+
+# Normal Distribution of the error (error is 1 std)
