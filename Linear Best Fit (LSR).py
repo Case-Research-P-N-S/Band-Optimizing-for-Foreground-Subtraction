97b0dc3c8b6d363bd070829bdd01fc3a0ea1f6bf
nstarman <nstarman@users.noreply.github.com>
2014-10-05 14:33:43 -0400

    Update Linear Best Fit (LSR).py
    Turns it into an applicable function

--- Linear Best Fit (LSR).py
+++ Linear Best Fit (LSR).py
@@ -6,39 +6,27 @@ import scipy as sp
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-# Given X and Y values
-xList = []
-yList = []
 
-# finds number of values in xList and yList
-Nx = len(xList)
-Ny = len(yList)
-
-# error if xlist and ylist are different lengths
-if Nx != Ny:
-    print "Error: incompatible lists"
-    raise Exception("Incompatible List length")
-
-N = Nx
-
-#the degree of the polynomial equation (we have only solved for degree = 1)
-degree = input("Enter the degree of the fitting polynomial: ")
-
-if degree == 1:
+def linearFit(xList, yList):
+    # finds number of values in xList and yList
+    Nx = len(xList)
+    Ny = len(yList)
+    
+    # error if xlist and ylist are different lengths
+    if Nx != Ny:
+        print "Error: incompatible lists"
+        raise Exception("Incompatible List length")
+    
+    #the degree =1 of the polynomial equation 
     # given x-values, y-values, and relationship degree polyfit returns list of polynomial coefficients starting with coefficient with the highest power
-    p = np.polyfit(xList, yList, degree)
-
+    p = np.polyfit(xList, yList, 1)  
     b = p[0] # highest degree
-    a = p[degree] #lowest degree
+    a = p[1] #lowest degree
 
     # calculating correlation coefficient. r = b *(stdev yList / stdev xList)
     r = b / (np.std(yList)/np.std(xList))
     
     # calculating r-square value
     rSquare = np.square(r)
-
-    plt.plot(xList, yList)
-    plt.plot(xList, [a + b*x for x in xList])
-else:
-    print "Error: degree is not 1"
-    raise Exception("Unusable degree")
+    
+    return [a, b, rSquare]
