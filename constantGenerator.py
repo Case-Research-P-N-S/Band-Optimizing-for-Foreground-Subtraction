4038eb5c5b468d783b8b27f157feda4d022897ea
nstarman <nstarman@users.noreply.github.com>
2015-01-24 16:05:11 -0500

    Update constantGenerator.py

--- constantGenerator.py
+++ constantGenerator.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
 #------------------------------------------
 # Creating Test Data
@@ -11,17 +13,18 @@ xList = [x for x in range(10)]
 errorYList = [2.0 for i in range(10)]
 
 # creating test Ylists. These arrays will actually be generated from given functions Y1, Y2, Y3, Y4, etc.
-Y1List = [1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0]
-#Y2List = [10,9,8,7,6,5,4,3,2,1]
+Y1List = [x**1.9 for x in xList]
+Y2List = [5.67**(10**-8)*(x**4) for x in xList]
 #Y3List = [5,6,7,8,9,10,1,2,3,4]
 #Y4List = [4,3,2,1,10,9,8,7,6,6]
+testY = [x for x in xList]
 
 #------------------------------------------
 # Matrix Function
 
 
 # this just makes it easier to refer to all the YLists
-YList = [Y1List] #, Y2List, Y3List, Y4List]
+YList = [testY] #[Y1List, Y2List, Y3List, Y4List]
 
 # Creates the A matrix for use in determining the constants
 def matrixFunction(functionList, errorYList):
@@ -44,6 +47,7 @@ matrixA = matrixFunction(YList, errorYList)
 
 # initialization and assignment of vector b
 vectorB = np.empty(len(yMeasured))
+
 for y, s, i in zip(yMeasured, errorYList, range(len(yMeasured))):
     vectorB[i] = y/s
 
@@ -54,8 +58,10 @@ tempA = np.dot(matrixA.T, matrixA)
 tempB = tempA.I
 
 # Dot Product of matrix and b vector
-tempC = np.dot(matrixA.T, tempB)
+tempC = np.dot(matrixA.T, vectorB)
 
-finalConstants = np.dot(tempC, vectorB)
+finalConstants = np.dot(tempC, tempB)
+constant = np.array(finalConstants)[0][0]
 
-print finalConstants
+plt.plot(xList, testY)
+plt.plot(xList, [x*constant for x in xList])
