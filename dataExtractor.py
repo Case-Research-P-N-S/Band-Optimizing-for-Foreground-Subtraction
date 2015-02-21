81d840bd2bb83b9b7e0b63df26fade697af2c977
smithy545 <smithy545@gmail.com>
2015-02-21 17:00:00 -0500

    Created function to isolate the third line of Lambda output tetx

--- dataExtractor.py
+++ dataExtractor.py
@@ -1,1 +1,13 @@
-test
+from numpy import genfromtxt
+
+outputFile = open("LAMDA Data BB", "w")
+
+data = genfromtxt("Lambda Data")
+print data
+
+for i in data:
+    outputFile.write(str(i[3]))
+    outputFile.write('\n')
+
+outputFile.close()
+dataFile.close()
