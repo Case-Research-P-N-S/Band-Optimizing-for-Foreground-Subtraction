79780aaad2fb346fd76860527f13e2b1cb8535ec
smithy545 <smithy545@gmail.com>
2015-03-22 18:09:38 -0400

    Added several functions Turned many lists into function format Added constants Redid YLists

--- GeneralizedLeastSquaresFit.py
+++ GeneralizedLeastSquaresFit.py
@@ -4,28 +4,54 @@ import matplotlib.pyplot as plt
 from dataExtractor import extractData
 
 #------------------------------------------
-# Creating Test Data
+# Creating functions
 
 # making a list with all the angle l values on the x-axis
-angles = [l for l in range(2, 1501)]
+xAxis = [l for l in range(2, 1501)]
+frequency = [x*(10**9) for x in xAxis]
 
 #errorYList = [np.random.normal(0, 0.1) for y in range(10)]
-errorYList = [0.1 for i in angles]
+errorYList = [0.1 for i in xAxis]
 
-# creating test Ylists. These arrays will actually be generated from given functions Y1, Y2, Y3, Y4, etc.
-Y1List = [l**1.9 for l in angles]
-Y2List = [5.67**(10**-8)*(l**4) for l in angles]
-#Y3List = [5,6,7,8,9,10,1,2,3,4]
-#Y4List = [4,3,2,1,10,9,8,7,6,6]
 
-yMeasured = extractData("LAMDA Data")
+# Reference table of constants
+h = 6.62606957*(10**-34)    # Plancks constant
+nu = 90*(10**9)             # Frequency
+c = 299792458               # Speed of Light
+k = 1.3806488*(10**-23)     # Boltzmann constant
+TDust = 19.6                # Temperature of dust in Kelvin
+TVac = 2.7                  # Temperature of vacuum in Kelvin
+nu0 = 1                     # Arbitrary value to fill space in function
+
+# Equation Functions
+def dustFreqPowLaw(nu, nu0 = 1):
+    return (nu/nu0)**1.59
+
+def blackbodyConvertofNu(nu, h = 6.62606957*(10**-34), c = 299792458, k = 1.3806488*(10**-23), T = 2.7):
+    return 2*(h**2)*(nu**4)*np.exp((h*nu)/(k*T))/(k*(T**2)*(c**2)*((np.exp((h*nu)/(k*T)) - 1)**2))
+
+def blackbody(nu, h = 6.62606957*(10**-34), c = 299792458, k = 1.3806488*(10**-23), T = 2.7):
+    return (2*(h*(nu**3))/(c**2))*(1/(np.exp((h*(nu))/(k*T)) - 1))
+
+def dustofL(l):
+    return (l/80.0)**-0.42
+
+def dustRatio(nu1, nu2):
+    return (dustFreqPowLaw(nu1)*blackbodyConvertofNu(nu2))/(dustFreqPowLaw(nu2)*blackbodyConvertofNu(nu1))
+
+
+# Function lists
+dustofLList = [dustofL(l) for l in xAxis]              # Dust of l
+blackbodyList = [blackbody(nu) for nu in frequency]    # Blackbody of (nu,T)
+BBofL = extractData("LAMDA Data")                      # BB(l)
+
 
 #------------------------------------------
 # Matrix Function
 
 
 # this just makes it easier to refer to all the YLists
-YList = [Y1List, Y2List] #[Y1List, Y2List, Y3List, Y4List]
+YList = [dustofLList, blackbodyList]
 
 # Creates the A matrix for use in determining the constants
 def matrixFunction(functionList, errorYList):
@@ -42,7 +68,6 @@ def matrixFunction(functionList, errorYList):
             tempList[j] = functionList[j][i]/s
         resultMatrix[i] = tempList
     return resultMatrix
-
 
 # initialization of Matrix A
 matrixA = matrixFunction(YList, errorYList)
@@ -65,6 +90,8 @@ tempC = np.dot(matrixA.T, vectorB)
 finalTemp = np.dot(tempC, tempB)
 vectorA = np.array(finalTemp)[0]
 print vectorA
-plt.plot([l for l in range(2,1501)],yMeasured)
-plt.plot([l for l in range(2,1501)],Y1List)
-plt.plot([l for l in range(2,1501)],Y2List)
+
+#plt.plot([l for l in angles],yMeasured)
+plt.plot(xAxis, [y*vectorA[0] for y in dustofLList])
+plt.plot(xAxis, [y*vectorA[1] for y in blackbodyList])
+#plt.plot(np.log(xAxis), np.log([y*z for y,z in zip([y*vectorA[0] for y in dustofL],[y*vectorA[1] for y in Blackbody])]))
